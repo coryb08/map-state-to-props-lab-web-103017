3ced2f0cf5ad9aa03c0b2ae63fa9f4ce799e6ee1
github username <github email address>
2018-03-06 17:34:08 -0500

    Done.

--- src/components/UserInput.js
+++ src/components/UserInput.js
@@ -1,54 +1,57 @@
-import React, { Component } from 'react';
-import { addUser } from '../actions/users';
+import React, { Component } from "react"
+import { addUser } from "../actions/users"
+import actions from "../actions/users.js"
+import { connect } from "react-redux"
 
 class UserInput extends Component {
-
   constructor(props) {
-    super(props);
-    
+    super(props)
+
     this.state = {
-      userName: '', 
-      hometown: ''
-    };
+      name: "",
+      hometown: ""
+    }
   }
 
   handleOnUserNameChange(event) {
     this.setState({
-      userName: event.target.value
-    });
+      name: event.target.value
+    })
   }
 
   handleOnHometownChange(event) {
     this.setState({
       hometown: event.target.value
-    });
+    })
   }
 
   handleOnSubmit(event) {
-    event.preventDefault();
-    
+    event.preventDefault()
+
     this.props.store.dispatch(addUser(this.state))
   }
 
   render() {
-    return(
-      <form onSubmit={(event) => this.handleOnSubmit(event)}>
-      <p>
-        <input 
-          type="text" 
-          onChange={(event) => this.handleOnUserNameChange(event)} 
-          placeholder="user name"/>
-      </p>
-      <p>
-        <input 
-          type="text" 
-          onChange={(event) => this.handleOnHometownChange(event)} 
-          placeholder="hometown"/>
-      </p>
+    return (
+      <form onSubmit={event => this.handleOnSubmit(event)}>
+        <p>
+          <input
+            type="text"
+            onChange={event => this.handleOnUserNameChange(event)}
+            placeholder="user name"
+          />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={event => this.handleOnHometownChange(event)}
+            placeholder="hometown"
+          />
+        </p>
         <input type="submit" />
       </form>
     )
   }
 }
 
-export default UserInput;
+export default UserInput
